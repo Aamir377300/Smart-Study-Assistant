39194b9b0b067db31ceb7ee06e18922f075eee9b
Aamir377300 <belalaamirkhan@gmail.com>
2025-11-14 05:04:06 +0530

    minor

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ const FRONTEND_URL2 = process.env.FRONTEND_URL2;
 app.use(
   cors({
     origin: [
-      process.env.FRONTEND_URL1,
-      process.env.FRONTEND_URL2
+      process.env.FRONTEND_URL,
+      "http://localhost:5173"
     ],
     methods: ["GET", "POST"],
     credentials: true
